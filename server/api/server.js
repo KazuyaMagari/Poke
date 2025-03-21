1e4e6bf765d9b282f9e29b8f24fdcc75f858fc08
KazuyaMagari <kazuyamagarifuchi1211@gmail.com>
2025-03-21 04:45:24 -0400

    test deploying

--- server/api/server.js
+++ server/api/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const serverless = require('serverless-http');
+
 const app = express();
-const PORT = 3000;
 
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send('hello');
-})
-app.listen(PORT, ()=>{
-    console.log('Open');
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Express on Vercel!' });
 });
+
+app.post('/api/data', (req, res) => {
+  const { name } = req.body;
+  res.json({ message: `Received data for ${name}` });
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
