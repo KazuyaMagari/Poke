ba99c78ef4c63a18d365605196b280c7dce588da
KazuyaMagari <kazuyamagarifuchi1211@gmail.com>
2025-03-21 18:03:48 -0400

    set the setting

--- server/api/server.js
+++ server/api/server.js
@@ -1,10 +1,9 @@
 import express from "express";
 import cors from "cors";
 import quizRouter from "../router/quizRouter.js"; // Ensure the import name matches
-const FAPI_URL =  process.env.VITE_API_FRONT_URL;
 const app = express();
 const corsOptions = {
-  origin: `${FAPI_URL}`, // Replace with your frontend API_URL",
+  origin: `"https://poke-v456.vercel.app"`, // Replace with your frontend API_URL",
 };
 
 const PORT = process.env.PORT || 3000;
