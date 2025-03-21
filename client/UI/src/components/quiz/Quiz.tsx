42f1ed4524e2f37fd13e066ad6294fe18a629002
KazuyaMagari <kazuyamagarifuchi1211@gmail.com>
2025-03-21 17:31:38 -0400

    again

--- client/UI/src/components/quiz/Quiz.tsx
+++ client/UI/src/components/quiz/Quiz.tsx
@@ -1,30 +1,31 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-const API_URL =  import.meta.env.VITE_API_BASE_URL;
+// import axios from 'axios';
+// import { useState, useEffect } from 'react';
+// const API_URL =  import.meta.env.VITE_API_BASE_URL;
 
 
 function Quiz() {
-    const [quiz, setQuiz] = useState(""); 
+    // const [quiz, setQuiz] = useState(""); 
 
-    const getQuiz = async () => {
-        try {
-            const res = await axios.get(`${API_URL}/api/quiz/image/1`);
-            setQuiz(res.data.url);
-        } catch (error) {
-            console.error("Error fetching quiz:", error);
-            setQuiz("クイズの取得に失敗しました。");
-        }
+    // const getQuiz = async () => {
+    //     try {
+    //         const res = await axios.get(`${API_URL}/quiz/image/1`);
+    //         setQuiz(res.data.url);
+    //     } catch (error) {
+    //         console.error("Error fetching quiz:", error);
+    //         setQuiz("クイズの取得に失敗しました。");
+    //     }
 
-    };
+    // };
 
-    useEffect(() => {
-        getQuiz();
-    }, []);
+    // useEffect(() => {
+    //     getQuiz();
+    //     console.log("API URL:", API_URL)    
+    // }, []);
 
     return (
         <div>
             <h1>クイズ</h1>
-            <img src={quiz} alt="Pokemon" />
+            {/* <img src={quiz} alt="Pokemon" /> */}
         </div>
     );
 }
