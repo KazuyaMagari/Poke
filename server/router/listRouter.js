8c14d5b53750e0bcb659adf81706b8c1a1c412de
KazuyaMagari <kazuyamagarifuchi1211@gmail.com>
2025-03-24 15:35:02 -0400

    test

--- server/router/listRouter.js
+++ server/router/listRouter.js
@@ -2,9 +2,9 @@ import express from "express";
 const  listRouter= express.Router();
 import ListController from "../controller/ListController.js";
 listRouter.get("/", (req, res) => {
-    res.json({ message: "Hello World" });
+    res.json({ message: "Hello World from list" });
 });
 
-listRouter.post("/:userId/:index", ListController.addList);
+listRouter.post("/", ListController.addList);
 
 export default listRouter
